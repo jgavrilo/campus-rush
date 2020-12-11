af920aacbc1280e96b04ac5eaa95708c0b06a87a
Dounglan Cheung <49924135+dcheung15@users.noreply.github.com>
2020-12-11 15:57:07 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,114 +9,6 @@ function myFunction(numQuestions) {
   }
 }
 
-/* //quiz game logic 11.18.2020
-//HTML DOM Objects = Start BUTTON, Next BUTTONS, Question container, Question element, answer element
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-let shuffledQuestions, currentQuestionIndex
-
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
-
-function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-}
-
-function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-}
-
-function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-  }
-}
-
-function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
-  } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
-  }
-}
-
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
-  } else {
-    element.classList.add('wrong')
-  }
-}
-
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-}
-
-//questions
-const questions = [{
-  question: "What is 2 x 2?",
-  answers: [
-    {text: '3', correct: false },
-    {text: '2', correct: false },
-    {text: '5', correct: false },
-    {text: '4', correct: true }
-]},
-{
-  question: 'When is New Years in the US?',
-  answers: [
-    {text: 'January 5', correct: false },
-    {text: 'December 25', correct: false },
-    {text: 'January 1', correct: true },
-    {text: 'February 14', correct: false }
-  ]},
-{
-  question: 'What is 53 + 26?',
-  answers: [
-    {text: '50', correct: false },
-    {text: '70', correct: false },
-    {text: '79', correct: true },
-    {text: '84', correct: false }
-  ]}
-] //end of questions array 
-*/
-
-//------11.29.2020 new version---------//
 //HTML DOM Objects
 var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
@@ -231,7 +123,7 @@ function beginQuiz() {
 function showScore() {
     quiz.style.display = "none";
     scoreBlock.style.display = "block";
-    scoreBlock.innerHTML = "<p> You scored " + score + " out of 10!</p>";
+    scoreBlock.innerHTML = "<p> You scored " + score + " out of " + questions.length + "!</p>";
 
     if (score < 4) {
         scoreBlock.innerHTML = "<p><br/>Not so good!</p>";
