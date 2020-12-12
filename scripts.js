a3a3f0ba2a31a7ad077cd6f49eb59c636dd7ff1c
WHesser <60201323+WHesser@users.noreply.github.com>
2020-12-11 22:19:19 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ var scoreBlock = document.getElementById("scoreBlock");
 var scoreMessage = document.getElementById("scoreMessage");
 var quizAgain = document.getElementById("quizAgain");
 
-let questions = [{
+var questions = [{
     question: "What is 1 + 1?",
     choiceA: "1",
     choiceB: "2",
@@ -110,24 +110,26 @@ function callPhpFile(){
   xhr.send();
   xhr.onload = function () {
     var javar = this.response;
-    console.log(javar);
-  };
-  xhr.onload = function() {
+    console.log("Loaded: "+javar);
+    
     if (xhr.status != 200) {
-        alert('Error ${xhr.status}: ${xhr.statusText}'); // e.g. 404: Not Found
+        alert('Error '+xhr.status+': '+xhr.statusText); // e.g. 404: Not Found
     } else { // xhr.responseXML is the server response
         var questDoc, qList, qs, as, i;
         questDoc = xhr.responseXML;
-        qs = questDoc.getElementsById("question");
-        as = questDoc.getElementsById("answer");
-        qList = [];
-        for (i = 0; i < x.length; i++){
-          qList.push({question: qs[i].nodeValue,
-            choiceA: as[i*5+0].nodeValue,
-            choiceB: as[i*5+1].nodeValue,
-            choiceC: as[i*5+2].nodeValue,
-            choiceD: as[i*5+3].nodeValue,
-            correctAnswer: as[i*5+4].nodeValue});
+        console.log('Question Document: '+questDoc);
+        if (questDoc != null) { //update questions from default
+		  qs = questDoc.getElementsById("question");
+          as = questDoc.getElementsById("answer");
+          qList = [];
+          for (i = 0; i < x.length; i++){
+            qList.push({question: qs[i].nodeValue,
+              choiceA: as[i*5+0].nodeValue,
+              choiceB: as[i*5+1].nodeValue,
+              choiceC: as[i*5+2].nodeValue,
+              choiceD: as[i*5+3].nodeValue,
+              correctAnswer: as[i*5+4].nodeValue});
+          }
         }
     }
   };
@@ -135,6 +137,7 @@ function callPhpFile(){
 
 //getQuestion function
 function getQuestion() {
+	callPhpFile();
   choiceResponse.style.display = "none";
   let q = questions[questionIndex];
   quizQuestion.innerHTML = "<p>Question " +(questionIndex+1) + ": " + q.question + "</p>";
